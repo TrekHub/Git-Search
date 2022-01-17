f2036259c7c177be6607501653ccadb9faa20379
TrekHub <teddyomondi001@gmail.com>
2022-01-17 23:43:50 +0300

    fixing the environment prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  token:  "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",
+
+  clientSecret:  "22258f853f6ad13ef5f238f32deb9e6b0cb56890",
+
+  clientid:  "467a2bbab7ef3c73d69d",
 };
